d42ce335595db49d0b74fc2cbc9e2c9ba90b99b3
Mykola <nikter387@gmail.com>
2024-04-15 01:32:56 +0300

    Commit 2. Writing a Person class.

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Person {
+
+    private String fullName;
+    private int age;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public Person(){}
+
+    public String move(){
+        return fullName + ", who is " + age + " years old, runs, jumps, walks and has fun!";
+    }
+
+    public String talk(){
+        return fullName + ", who is " + age + " years old, smiles, talks, jokes and grinds his tongue!";
+    }
+}
