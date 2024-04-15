d2f4ab944cc222288159fd597094cf709e46a3ad
Mykola <nikter387@gmail.com>
2024-04-15 10:19:04 +0300

    Commit 3. Successful completion of task 1.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Person personTom = new Person();
+        personTom.setFullName("Tomas Edison");
+        personTom.setAge(23);
+        System.out.println(personTom.move());
+        System.out.println(personTom.talk());
+
+        Person personJack = new Person("Jack Faster", 25);
+        System.out.println(personJack.move());
+        System.out.println(personJack.talk());
+
     }
 }
