e51a60b48491dc83fdb6b6fbd88253217f2cd71d
Mykola <nikter387@gmail.com>
2024-04-19 09:14:23 +0300

    Commit 4. Successful writing of the Phone class and completion of sixth lab task.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        //Task 1.
         Person personTom = new Person();
         personTom.setFullName("Tomas Edison");
         personTom.setAge(23);
@@ -11,6 +12,21 @@ public class Main {
         Person personJack = new Person("Jack Faster", 25);
         System.out.println(personJack.move());
         System.out.println(personJack.talk());
+
+        //Task 2.
+        Phone phoneHuawei = new Phone(799186430, "Huawei Honor 20 Pro", 174);
+        Phone phoneSamsung = new Phone(158341656, "Samsung Galaxy Note 20 Ultra", 208);
+        Phone phoneXiaomi = new Phone(730191503, "Xiaomi Redmi Note 8T", 200);
+        System.out.println("This is " + phoneHuawei.getModel() + ", his weight is " + phoneHuawei.getWeight() + ", his number is " + phoneHuawei.getNumber());
+        System.out.println("This is " + phoneSamsung.getModel() + ", his weight is " + phoneSamsung.getWeight() + ", his number is " + phoneSamsung.getNumber());
+        System.out.println("This is " + phoneXiaomi.getModel() + ", his weight is " + phoneXiaomi.getWeight() + ", his number is " + phoneXiaomi.getNumber());
+        System.out.println(phoneHuawei.receiveCall("Max") + "The phone number is " + phoneHuawei.getNumber() + ".");
+        System.out.println(phoneSamsung.receiveCall("Tom") + "The phone number is " + phoneSamsung.getNumber() + ".");
+        System.out.println(phoneXiaomi.receiveCall("Nick") + "The phone number is " + phoneXiaomi.getNumber() + ".");
+        System.out.println(phoneHuawei.sendMessage(781154124, 485455589, 788845652));
+        System.out.println(phoneSamsung.sendMessage(458816799, 482944395, 145888412, 188718166, 518811835));
+        System.out.println(phoneXiaomi.sendMessage(485684225, 471155655));
+
 
     }
 }
---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Phone {
+
+    private int number;
+    private String model;
+    private int weight;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int _number) {
+        this.number = _number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String _model) {
+        this.model = _model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int _weight) {
+        this.weight = _weight;
+    }
+
+    public Phone(int _number, String _model, int _weight) {
+        this.number = _number;
+        this.model = _model;
+        this.weight = _weight;
+    }
+
+    public Phone(int _number, String _model){
+        this.number = _number;
+        this.model = _model;
+    }
+
+    public Phone(){}
+
+    public String receiveCall(String _fullName){
+        return _fullName + " is calling you. ";
+    }
+
+    public String receiveCall(String _fullName, int _number){
+        return _fullName + " is calling you, his number is " + _number + ".";
+    }
+
+    public String sendMessage(int... _number){
+        return Arrays.toString(_number);
+    }
+}
