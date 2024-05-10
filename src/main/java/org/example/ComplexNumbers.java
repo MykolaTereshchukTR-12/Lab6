87e86db682b0c3fc9e7100c7061b99d60d2415fb
Mykola <nikter387@gmail.com>
2024-05-10 08:53:29 +0300

    Commit 9. Correction of the problem of entering values and optimization of the program. Code readability has also been improved due to comments and optimization.

--- src/main/java/org/example/ComplexNumbers.java
+++ src/main/java/org/example/ComplexNumbers.java
@@ -15,12 +15,84 @@ public class ComplexNumbers {
     public double getImagePart() { return imagePart; }
 
     public double module() { return (Math.sqrt((realPart * realPart) + (imagePart * imagePart))); }
-    public double argumentInRadian() { return (Math.atan((imagePart)/(realPart))); }
-    public double argumentInDegree() { return (((Math.atan((imagePart)/(realPart)))*(180))/(Math.PI)); }
+    public double argumentInRadian() {
+        if(realPart == 0){
+            if(imagePart > 0){
+                return (Math.PI/2);
+            } else if (imagePart < 0) {
+                return -(Math.PI/2);
+            } else {
+                return -1;
+            }
+        }
+
+        if(imagePart == 0){
+            if(realPart > 0){
+                return 0;
+            } else if (realPart < 0) {
+                return (Math.PI);
+            } else {
+                return -1;
+            }
+        }
+
+        if(realPart > 0){
+            return (Math.atan((imagePart)/(realPart)));
+        }else{
+            if(imagePart > 0){
+                return ((Math.atan((imagePart)/(realPart)))+(Math.PI));
+            }else{
+                return ((Math.atan((imagePart)/(realPart)))-(Math.PI));
+            }
+        }
+    }
+    public double argumentInDegree() {
+        if(realPart == 0){
+            if(imagePart > 0){
+                return 90;
+            } else if (imagePart < 0) {
+                return -90;
+            } else {
+                return -1;
+            }
+        }
+
+        if(imagePart == 0){
+            if(realPart > 0){
+                return 0;
+            } else if (realPart < 0) {
+                return 180;
+            } else {
+                return -1;
+            }
+        }
+
+        if(realPart > 0){
+            return (((Math.atan((imagePart)/(realPart)))*(180))/(Math.PI));
+        }else{
+            if(imagePart > 0){
+                return ((((Math.atan((imagePart)/(realPart)))*(180))/(Math.PI))+180);
+            }else{
+                return ((((Math.atan((imagePart)/(realPart)))*(180))/(Math.PI))-180);
+            }
+        }
+    }
 
     public String getNumber() { return "{" + realPart + " + " + imagePart + "i}"; }
     public String getModule() { return "The module of " + getNumber() + " is " + module() + "."; }
-    public String getArgumentInRadian() { return "The argument of " + getNumber() + " is " + argumentInRadian() + " in degrees."; }
-    public String getArgumentInDegree() { return "The argument of " + getNumber() + " is " + argumentInDegree() + " in degrees."; }
+    public String getArgumentInRadian() {
+        if (argumentInRadian() != (-1)) {
+            return "The argument of " + getNumber() + " is " + argumentInRadian() + " in radians.";
 
+        }else{
+            return "The complex number argument is undefined.";
+        }
+    }
+    public String getArgumentInDegree() {
+        if (argumentInDegree() != (-1)) {
+            return "The argument of " + getNumber() + " is " + argumentInDegree() + " in degrees.";
+        }else{
+            return "The complex number argument is undefined.";
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    private Scanner scanner = new Scanner(System.in);
+
+    private int _control = 0;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Initialization of required objects to prevent accumulation of unnecessary objects.
+        Main main = new Main();
         ComplexNumbers complexNumbers1 = new ComplexNumbers();
         ComplexNumbers complexNumbers2 = new ComplexNumbers();
         ComplexNumbersActions complexNumbersActions = new ComplexNumbersActions();
-        Main main = new Main();
-
+        Sedan sedan = new Sedan();
+        Truck truck = new Truck();
         String name = "";
         int age = 0;
+
+
 
         //Start the program
         System.out.println("\n=) Hello! This is a program that performs the tasks of laboratory work in the discipline \"Object Oriented Programming\". " +
@@ -22,30 +29,32 @@ public class Main {
 
         //Initialization of the eternal cycle
         while(true) {
-
             //Switch() declarations for different application modes
             switch(main.controlSYS()) {
 
                 //Exit the loop and the program as a whole
-                case (0): {
+                case ("0"): {
                     System.out.println("Before meeting!");
                     System.exit(0);
                 }
 
                 //Task 1.
-                case (1): {
+                case ("1"): {
                     Person personTom = new Person();
                     System.out.println("Enter your character's name. In the case of multi-word names, separate them with the \"_\" character:");
-                    personTom.setFullName(scanner.nextLine());
+
+                    personTom.setFullName(main.scanner.nextLine());
                     System.out.println("Enter your character's age:");
-                    personTom.setAge(scanner.nextInt());
+                    personTom.setAge(main.scanner.nextInt());
+                    main.scanner.nextLine();
                     System.out.println(personTom.move());
                     System.out.println(personTom.talk());
 
                     System.out.println("Enter your character's name. In the case of multi-word names, separate them with the \"_\" character:");
-                    name = scanner.nextLine();
+                    name = main.scanner.nextLine();
                     System.out.println("Enter your character's age:");
-                    age = scanner.nextInt();
+                    age = main.scanner.nextInt();
+                    main.scanner.nextLine();
                     Person personJack = new Person(name, age);
                     System.out.println(personJack.move());
                     System.out.println(personJack.talk());
@@ -54,7 +63,7 @@ public class Main {
                 }
 
                 //Task 2.
-                case (2): {
+                case ("2"): {
                     System.out.println("Let's look at the already set values and execution of the program written based on task 2:");
                     Phone phoneHuawei = new Phone(799186430, "Huawei Honor 20 Pro", 174);
                     Phone phoneSamsung = new Phone(158341656, "Samsung Galaxy Note 20 Ultra", 208);
@@ -73,18 +82,18 @@ public class Main {
                 }
 
                 //Task 3.
-                case (3): {
+                case ("3"): {
                     System.out.println("Enter the length of the radius of the circle:");
-                    Ball ball = new Ball(scanner.nextInt());
+                    Ball ball = new Ball(main.scanner.nextInt());
+                    main.scanner.nextLine();
                     System.out.println(ball.getVolume());
                     System.out.println("Do you want to continue? Enter 6 for help or 0 to exit the program.");
                     break;
                 }
 
                 //Task 4.
-                case (4): {
+                case ("4"): {
                     System.out.println("Let's look at the already set values and execution of the program written based on task 2:");
-                    Sedan sedan = new Sedan();
                     sedan.setModel("Honda Accord");
                     sedan.setColor("blue");
                     sedan.setMaxSpeed(100);
@@ -96,7 +105,6 @@ public class Main {
                     System.out.println(sedan.gas());
                     System.out.println(sedan.brake());
 
-                    Truck truck = new Truck();
                     truck.setModel("Scania R580");
                     truck.setColor("white");
                     truck.setMaxSpeed(110);
@@ -111,18 +119,18 @@ public class Main {
                 }
 
                 //Task 5 and 6.
-                case (5): {
+                case ("5"): {
                     System.out.println("Enter the real and imaginary parts of the first complex number:");
-                    complexNumbers1.setRealPart(scanner.nextInt());
-                    complexNumbers1.setImagePart(scanner.nextInt());
+                    complexNumbers1.setRealPart(main.scanner.nextInt());
+                    complexNumbers1.setImagePart(main.scanner.nextInt());
                     System.out.println(complexNumbers1.getNumber());
                     System.out.println(complexNumbers1.getModule());
                     System.out.println(complexNumbers1.getArgumentInRadian());
                     System.out.println(complexNumbers1.getArgumentInDegree());
 
                     System.out.println("Enter the real and imaginary parts of the second complex number:");
-                    complexNumbers2.setRealPart(scanner.nextInt());
-                    complexNumbers2.setImagePart(scanner.nextInt());
+                    complexNumbers2.setRealPart(main.scanner.nextInt());
+                    complexNumbers2.setImagePart(main.scanner.nextInt());
                     System.out.println(complexNumbers2.getNumber());
                     System.out.println(complexNumbers2.getModule());
                     System.out.println(complexNumbers2.getArgumentInRadian());
@@ -132,11 +140,13 @@ public class Main {
                     System.out.println(complexNumbersActions.getDifTwoNum(complexNumbers1.getRealPart(), complexNumbers1.getImagePart(), complexNumbers2.getRealPart(), complexNumbers2.getImagePart()));
                     System.out.println(complexNumbersActions.getMulTwoNum(complexNumbers1.getRealPart(), complexNumbers1.getImagePart(), complexNumbers2.getRealPart(), complexNumbers2.getImagePart()));
                     System.out.println(complexNumbersActions.getDivTwoNum(complexNumbers1.getRealPart(), complexNumbers1.getImagePart(), complexNumbers2.getRealPart(), complexNumbers2.getImagePart()));
+                    main.scanner.nextLine();
                     System.out.println("Do you want to continue? Enter 6 for help or 0 to exit the program.");
                     break;
                 }
 
-                case (6): {
+                //Help block
+                case ("6"): {
                     System.out.println("The essence of the program:");
                     System.out.println(main.help());
                     break;
@@ -144,7 +154,7 @@ public class Main {
 
                 //In case of error or unforeseen situations.
                 default: {
-                    System.out.println(main.ground());
+                    System.out.println("Please follow the instructions and be careful when working with this program.");
                     System.out.println(main.help());
                     break;
                 }
@@ -152,19 +162,19 @@ public class Main {
         }
     }
 
-    public int controlSYS() {
-        Scanner scanner = new Scanner(System.in);
-        int _control = 0;
+    //A block that controls and accompanies the program.
+    public String controlSYS() {
+        String _control = "0";
         try {
-            _control = scanner.nextInt();
+            _control = scanner.nextLine();
         } catch(InputMismatchException e) {
-            System.out.println("Please follow the instructions and be careful when working with this program.");
-            return 11;
+            System.out.println("Please follow the instructions and be careful when working with this program!!!");
+            _control = "6";
         }
         return _control;
     }
 
-    //Help block
+    //Instruction
     public String help() {
         return """
                 Enter 1 to display the result of task 1;\s
@@ -174,9 +184,4 @@ public class Main {
                 Enter 5 to display the result of tasks 5 and 6;\s
                 Enter 0 to exit the program.""";
     }
-
-    public String ground() {
-        return "Please follow the instructions and be careful when working with this program.";
-    }
-
 }
